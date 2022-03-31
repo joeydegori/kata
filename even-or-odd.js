d1e3d3604e9c7c849198cc308bedbd6cb4379517
joeydegori <96920177+joeydegori@users.noreply.github.com>
2022-03-30 21:16:30 -0700

    Finished Kata

---
+++ even-or-odd.js
@@ -0,0 +1,12 @@
+//Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+//https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+
+let number; 
+
+function even_or_odd(number) {
+  if(number % 2 === 0){
+    return ("Even")
+  } else {
+      return ("Odd")
+    }
+  }
